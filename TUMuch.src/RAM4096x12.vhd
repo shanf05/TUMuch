19bbdf1f4c28b75d70263c471980cc27b99a1d5d
임정주 <mr4117@outlook.kr>
2025-10-25 12:12:34 +0200

    finish Memory Model with 12-dimensional Index

--- TUMuch.src/RAM4096x12.vhd
+++ TUMuch.src/RAM4096x12.vhd
@@ -3,7 +3,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity RAM4096x12 is
+-- ENTITY DECLARATION
+entity RAM4096x12 is 
 port(
     w_en : in bit;
     addr : in bit_vector( 11 downto 0 );
@@ -12,7 +13,8 @@ port(
 );
 end RAM4096x12;
 
-architecture Behavioral of RAM4096x12 is
+--ARCHITECTURE BODY (for uut1)
+architecture Behavioral_BitVector of RAM4096x12 is
 begin
     process( w_en , addr , data_in )
     type mem_type is array
@@ -34,4 +36,4 @@ begin
     addr(5),addr(4),addr(3),
     addr(2),addr(1),addr(0));
     end process;
-end Behavioral;
+end Behavioral_BitVector;
