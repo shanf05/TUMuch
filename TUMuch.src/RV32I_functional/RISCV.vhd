de5c1b4d9ab5cd0ed3902d13224318c2f6f21706
Severin Hanf <Severin.Hanf@gmail.com>
2025-11-03 16:56:23 +0100

    add file structure and cosmetic vhd file

---
+++ TUMuch.src/RV32I_functional/RISCV.vhd
@@ -0,0 +1,11 @@
+entity RISCV is
+end RISCV;
+
+architecture functional of RISCV is
+begin
+    process
+-- include all necessary packages
+    begin
+-- fetch, decode and execute instructions
+    end process;
+end functional;
