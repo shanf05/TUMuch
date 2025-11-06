0683728896b77cfff925690575a879369f7bcdb1
Severin Hanf <Severin.Hanf@gmail.com>
2025-11-06 17:47:48 +0100

    add packages for executing procedures

---
+++ TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack.vhd
@@ -0,0 +1,4 @@
+
+package exec_procedures_pack is
+
+end package exec_procedures_pack;
---
+++ TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack_body.vhd
@@ -0,0 +1,6 @@
+library work;
+use work.exec_procedures_pack.all; 
+
+package body exec_procedures_pack is
+
+end package body exec_procedures_pack;
