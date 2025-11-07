08ad7f1303011e086b85ddf4a809385b7a51c9bd
Severin Hanf <Severin.Hanf@gmail.com>
2025-11-07 15:43:51 +0100

    add JAL_exec()

--- TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack_body.vhd
+++ TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack_body.vhd
@@ -2,22 +2,25 @@ library work;
 use work.exec_procedures_pack.all; 
 use work.defs_pack.all; 
 library ieee;
-use ieee.numeric_std.all;
+use IEEE.numeric_bit.all;
 
 package body exec_procedures_pack is   
 
-    procedure JAL_exec  (rd : RegAddrType; imm : RegDataType; mem : inout MemType; reg : inout RegType; pc : inout AddrType)    is                --Severin Hanf
+    procedure JAL_exec  (rd : RegAddrType; imm : RegDataType; mem : inout MemType; reg : inout RegType; pc : inout MemAddrType)    is                --Severin Hanf
+    variable tmp1 : bit_vector(19 downto 0) := (others=>'0'); 
+    variable tmp2 : integer := 0; 
     begin    
-        if(imm(20) = '0') then
-            pc := pc + unsigned( imm(10 downto 1) & imm(11) & imm(19 downto 12) & '0' );
+        reg(rd) := bit_vector(to_unsigned(pc + 4,32) ); --store the return address in rd              
+        
+        if(imm(20) = '0') then            
+            pc := pc + to_integer(unsigned(imm(10 downto 1) & imm(11) & imm(19 downto 12) & '0'));
         else 
-            pc := pc + signed();
-        end if;
-               
-        reg(rd) := pc + 4;        --store the return address in rd
+            pc := pc + to_integer(signed(imm(10 downto 1) & imm(11) & imm(19 downto 12) & '0'));
+        end if;               
+        
     end procedure;              
     
-    procedure JALR_exec (rs1, rd : RegAddrType; imm : RegDataType; mem : MemType; regs : RegType; pc : AddrType) is           --Severin Hanf
+    procedure JALR_exec (rs1, rd : RegAddrType; imm : RegDataType; mem : inout MemType; reg : inout RegType; pc : inout MemAddrType) is           --Severin Hanf
     begin
     
     end procedure; 
