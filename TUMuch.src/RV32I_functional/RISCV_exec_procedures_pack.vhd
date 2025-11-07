6b59e966f79496ada7e5a354a7af66d687b964f9
Severin Hanf <Severin.Hanf@gmail.com>
2025-11-07 14:25:11 +0100

    add new procedures, not compilable yet. Rebase.

--- TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack.vhd
+++ TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack.vhd
@@ -1,4 +1,9 @@
+library work;
+use work.defs_pack.all;
 
 package exec_procedures_pack is
 
+    procedure JAL_exec  (rd : RegAddrType; imm : RegDataType; mem : inout MemType; reg : inout RegType; pc : inout AddrType);              --Severin Hanf
+    procedure JALR_exec (rs1, rd : RegAddrType; imm : RegDataType; mem : inout MemType; reg : inout RegType; pc : inout AddrType);         --Severin Hanf
+    
 end package exec_procedures_pack;
--- TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack_body.vhd
+++ TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack_body.vhd
@@ -1,6 +1,25 @@
 library work;
 use work.exec_procedures_pack.all; 
+use work.defs_pack.all; 
+library ieee;
+use ieee.numeric_std.all;
 
-package body exec_procedures_pack is
+package body exec_procedures_pack is   
 
+    procedure JAL_exec  (rd : RegAddrType; imm : RegDataType; mem : inout MemType; reg : inout RegType; pc : inout AddrType)    is                --Severin Hanf
+    begin    
+        if(imm(20) = '0') then
+            pc := pc + unsigned( imm(10 downto 1) & imm(11) & imm(19 downto 12) & '0' );
+        else 
+            pc := pc + signed();
+        end if;
+               
+        reg(rd) := pc + 4;        --store the return address in rd
+    end procedure;              
+    
+    procedure JALR_exec (rs1, rd : RegAddrType; imm : RegDataType; mem : MemType; regs : RegType; pc : AddrType) is           --Severin Hanf
+    begin
+    
+    end procedure; 
+    
 end package body exec_procedures_pack;
