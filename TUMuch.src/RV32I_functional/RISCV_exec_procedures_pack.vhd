e6cdfc2b743f3a9d06c89fa11461c5124b66cf3a
임정주 <mr4117@outlook.kr>
2025-11-08 15:35:42 +0100

    setup for load instr

--- TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack.vhd
+++ TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack.vhd
@@ -4,11 +4,11 @@ use work.defs_pack.all;
 package exec_procedures_pack is
 
 -- ERSTELLT VON JEONGJOO LIM; Teil Orange; LB LBU LH LHU LW SB SH SW ADD SUB ADDI
-    procedure LB_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType);
-    procedure LBU_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType);
-    procedure LH_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType);
-    procedure LHU_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType);
-    procedure LW_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType);
+    procedure LB_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType);
+    procedure LBU_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType);
+    procedure LH_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType);
+    procedure LHU_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType);
+    procedure LW_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType);
     procedure SB_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType);
     procedure SH_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType);
     procedure SW_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType);
--- TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack_body.vhd
+++ TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack_body.vhd
@@ -7,29 +7,42 @@ use work.exec_procedures_pack.all;
 package body exec_procedures_pack is
 
 -- ERSTELLT VON JEONGJOO LIM; Teil Orange; LB LBU LH LHU LW SB SH SW ADD SUB ADDI
-    procedure LB_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType) is
+    procedure LB_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
     begin
         
     end procedure;
     
-    procedure LBU_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType) is
+    procedure LBU_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
     begin
         
     end procedure;
     
-    procedure LH_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType) is
+    procedure LH_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
+    begin
+        
+    end procedure;
+        
+    procedure LHU_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
     begin
         
     end procedure;
     
-    procedure LHU_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType) is
+    procedure LW_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
     begin
-        
-    end procedure;
-    
-    procedure LW_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType) is
-    begin
-        
+        case rs1 mod 4 is
+            when 0 => 
+                case to_integer(signed(imm)) is
+                    when 0 => Reg(rs1) := Mem(rs1);
+                    when others =>
+                        assert FALSE
+                        report "Illegal Imm -- LW_exec"
+                        severity error;
+                end case;                 
+            when others =>                                  
+                assert FALSE
+                report "Illegal Address -- LW_exec"
+                severity error;
+        end case;
     end procedure;
     
     procedure SB_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType) is
@@ -44,22 +57,28 @@ package body exec_procedures_pack is
     
     procedure SW_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType) is
     begin
-        
+    
     end procedure;
     
     procedure ADD_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType) is
+        variable temp : signed(RegDataSize-1 downto 0);
     begin
-        Reg(rd) := bit_vector(signed(Reg(rs1)) + signed(Reg(rs2)));
+        temp := signed(Reg(rs1)) + signed(Reg(rs2));
+        Reg(rd) := bit_vector(temp);
     end procedure;
     
     procedure SUB_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType) is
+        variable temp : signed(RegDataSize-1 downto 0);
     begin
-        Reg(rd) := bit_vector(signed(Reg(rs1)) - signed(Reg(rs2)));
+        temp := signed(Reg(rs1)) - signed(Reg(rs2));
+        Reg(rd) := bit_vector(temp);
     end procedure;
     
     procedure ADDI_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
+        variable temp : signed(RegDataSize-1 downto 0);
     begin
-        --Reg(rd) := bit_vector(signed(Reg(rs1)) + signed((31 downto 12 => imm(11)) & imm ));
+        temp := signed(Reg(rs1)) + resize(signed(imm), RegDataSize);
+        Reg(rd) := bit_vector(temp);
     end procedure;
     
     
