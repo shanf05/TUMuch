06ffeccd48d281e3882eeb69280e2ccce72da6f5
CraftJJGamer308 <mr4117@outlook.kr>
2025-11-09 13:18:05 +0100

    finish orange part (hopefully)

--- TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack_body.vhd
+++ TUMuch.src/RV32I_functional/RISCV_exec_procedures_pack_body.vhd
@@ -10,22 +10,17 @@ package body exec_procedures_pack is
     procedure LB_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
         variable temp : signed(RegDataSize-1 downto 0);
     begin
-        if rs1 mod 4 = 0 then
-            case to_integer(unsigned(imm)) is
-                when 0 => temp := signed(Mem(rs1)(7 downto 0));
-                when 1 => temp := signed(Mem(rs1)(15 downto 8));
-                when 2 => temp := signed(Mem(rs1)(23 downto 16));
-                when 3 => temp := signed(Mem(rs1)(31 downto 24));
-                when others =>
-                    assert FALSE
-                    report "Illegal Imm -- LB_exec"
-                    severity error;
-            end case;
-        else
-            assert FALSE
-            report "Illegal Address -- LB_exec"
-            severity error;
+        if rs1 mod 4 /= 0 then
+            report "Address misalignment -- LB_exec"
+            severity warning;
         end if;
+        
+        case (rs1 + to_integer(signed(imm))) mod 4 is
+            when 0 => temp := signed(Mem((rs1 + to_integer(signed(imm))) / 4) (7 downto 0));
+            when 1 => temp := signed(Mem((rs1 + to_integer(signed(imm))) / 4) (15 downto 8));
+            when 2 => temp := signed(Mem((rs1 + to_integer(signed(imm))) / 4) (23 downto 16));
+            when 3 => temp := signed(Mem((rs1 + to_integer(signed(imm))) / 4) (31 downto 24));
+        end case;
         
         Reg(rd) := bit_vector(temp);
     end procedure;
@@ -33,22 +28,17 @@ package body exec_procedures_pack is
     procedure LBU_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
         variable temp : unsigned(RegDataSize-1 downto 0);
     begin
-        if rs1 mod 4 = 0 then
-            case to_integer(unsigned(imm)) is
-                when 0 => temp := unsigned(Mem(rs1)(7 downto 0));
-                when 1 => temp := unsigned(Mem(rs1)(15 downto 8));
-                when 2 => temp := unsigned(Mem(rs1)(23 downto 16));
-                when 3 => temp := unsigned(Mem(rs1)(31 downto 24));
-                when others =>
-                    assert FALSE
-                    report "Illegal Imm -- LBU_exec"
-                    severity error;
-            end case;
-        else
-            assert FALSE
-            report "Illegal Address -- LBU_exec"
-            severity error;
+        if rs1 mod 4 /= 0 then
+            report "Address misalignment -- LBU_exec"
+            severity warning;
         end if;
+        
+        case (rs1 + to_integer(signed(imm))) mod 4 is
+            when 0 => temp := unsigned(Mem((rs1 + to_integer(signed(imm))) / 4) (7 downto 0));
+            when 1 => temp := unsigned(Mem((rs1 + to_integer(signed(imm))) / 4) (15 downto 8));
+            when 2 => temp := unsigned(Mem((rs1 + to_integer(signed(imm))) / 4) (23 downto 16));
+            when 3 => temp := unsigned(Mem((rs1 + to_integer(signed(imm))) / 4) (31 downto 24));
+        end case;
         
         Reg(rd) := bit_vector(temp);
     end procedure;
@@ -56,20 +46,19 @@ package body exec_procedures_pack is
     procedure LH_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
         variable temp : signed(RegDataSize-1 downto 0);
     begin
-        if rs1 mod 4 = 0 then
-            case to_integer(unsigned(imm)) is
-                when 0 => temp := signed(Mem(rs1)(15 downto 0));
-                when 2 => temp := signed(Mem(rs1)(32 downto 16));
-                when others =>
-                    assert FALSE
-                    report "Illegal Imm -- LH_exec"
-                    severity error;
-            end case;
-        else
-            assert FALSE
-            report "Illegal Address -- LH_exec"
-            severity error;
+        if rs1 mod 4 /= 0 then
+            report "Address misalignment -- LH_exec"
+            severity warning;
         end if;
+        
+        case (rs1 + to_integer(signed(imm))) mod 4 is
+            when 0 => temp := signed(Mem((rs1 + to_integer(signed(imm))) / 4) (15 downto 0));
+            when 2 => temp := signed(Mem((rs1 + to_integer(signed(imm))) / 4) (31 downto 16));
+            when others =>
+                assert FALSE
+                report "Illegal Imm -- LH_exec"
+                severity error;
+        end case;
         
         Reg(rd) := bit_vector(temp);
     end procedure;
@@ -77,66 +66,90 @@ package body exec_procedures_pack is
     procedure LHU_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
         variable temp : unsigned(RegDataSize-1 downto 0);
     begin
-        if rs1 mod 4 = 0 then
-            case to_integer(unsigned(imm)) is
-                when 0 => temp := unsigned(Mem(rs1)(15 downto 0));
-                when 2 => temp := unsigned(Mem(rs1)(32 downto 16));
-                when others =>
-                    assert FALSE
-                    report "Illegal Imm -- LHU_exec"
-                    severity error;
-            end case;
-        else
-            assert FALSE
-            report "Illegal Address -- LHU_exec"
-            severity error;
+        if rs1 mod 4 /= 0 then
+            report "Address misalignment -- LHU_exec"
+            severity warning;
         end if;
+        
+        case (rs1 + to_integer(signed(imm))) mod 4 is
+            when 0 => temp := unsigned(Mem((rs1 + to_integer(signed(imm))) / 4) (15 downto 0));
+            when 2 => temp := unsigned(Mem((rs1 + to_integer(signed(imm))) / 4) (31 downto 16));
+            when others =>
+                assert FALSE
+                report "Illegal Imm -- LHU_exec"
+                severity error;
+        end case;
         
         Reg(rd) := bit_vector(temp);
     end procedure;
     
     procedure LW_exec (rd, rs1 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
+        variable temp : signed(RegDataSize-1 downto 0);
     begin
-        if rs1 mod 4 = 0 then
-            if to_integer(unsigned(imm)) = 0 then
-                Reg(rd) := Mem(rs1);
-            else
-                assert FALSE
-                report "Illegal Imm -- LW_exec"
-                severity error;
-            end if;
-        else
-            assert FALSE
-            report "Illegal Address -- LW_exec"
-            severity error;
+        if rs1 mod 4 /= 0 then
+            report "Address misalignment -- LB_exec"
+            severity warning;
         end if;
+        
+        case (rs1 + to_integer(signed(imm))) mod 4 is
+            when 0 => temp := signed(Mem((rs1 + to_integer(signed(imm))) / 4));
+            when others =>
+                assert FALSE
+                report "Illegal Imm -- LB_exec"
+                severity error;
+        end case;
+        
+        Reg(rd) := bit_vector(temp);
     end procedure;
     
     procedure SB_exec (rs1, rs2 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
     begin
+        if rs1 mod 4 /= 0 then
+            report "Address misalignment -- SB_exec"
+            severity warning;
+        end if;
+        
+        case (rs1 + to_integer(signed(imm))) mod 4 is
+            when 0 => Mem((rs1 + to_integer(signed(imm))) / 4) (7 downto 0)     := Reg(rs2)(7 downto 0);
+            when 1 => Mem((rs1 + to_integer(signed(imm))) / 4) (15 downto 8)    := Reg(rs2)(7 downto 0);
+            when 2 => Mem((rs1 + to_integer(signed(imm))) / 4) (23 downto 16)   := Reg(rs2)(7 downto 0);
+            when 3 => Mem((rs1 + to_integer(signed(imm))) / 4) (32 downto 24)   := Reg(rs2)(7 downto 0);
+        end case;
         
     end procedure;
     
     procedure SH_exec (rs1, rs2 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
     begin
-    
+        if rs1 mod 4 /= 0 then
+            report "Address misalignment -- SH_exec"
+            severity warning;
+        end if;
+        
+        case (rs1 + to_integer(signed(imm))) mod 4 is
+            when 0 => Mem((rs1 + to_integer(signed(imm))) / 4) (15 downto 0)     := Reg(rs2)(15 downto 0);
+            when 2 => Mem((rs1 + to_integer(signed(imm))) / 4) (31 downto 16)   := Reg(rs2)(15 downto 0);
+            when others =>
+                assert FALSE
+                report "Illegal Imm -- SH_exec"
+                severity error;
+        end case;
+        
     end procedure;
     
     procedure SW_exec (rs1, rs2 : RegAddrType; imm : ImmType; Reg : inout RegType; Mem : inout MemType) is
     begin
-        if rs1 mod 4 = 0 then
-            if to_integer(unsigned(imm)) = 0 then
-                Mem(rs1) := Reg(rs2); -- problematsich
-            else
+        if rs1 mod 4 /= 0 then
+            report "Address misalignment -- SW_exec"
+            severity warning;
+        end if;
+        
+        case (rs1 + to_integer(signed(imm))) mod 4 is
+            when 0 => Mem((rs1 + to_integer(signed(imm))) / 4) := Reg(rs2);
+            when others =>
                 assert FALSE
                 report "Illegal Imm -- SW_exec"
                 severity error;
-            end if;
-        else
-            assert FALSE
-            report "Illegal Address -- SW_exec"
-            severity error;
-        end if;
+        end case;
     end procedure;
     
     procedure ADD_exec (rd, rs1, rs2 : RegAddrType; Reg : inout RegType; Mem : inout MemType) is
