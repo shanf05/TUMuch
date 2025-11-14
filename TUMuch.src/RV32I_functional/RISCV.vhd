391c1f630db45f434cb7d234e771c9810044175a
Severin Hanf <Severin.Hanf@gmail.com>
2025-11-14 11:56:29 +0100

    minor changes

--- TUMuch.src/RV32I_functional/RISCV.vhd
+++ TUMuch.src/RV32I_functional/RISCV.vhd
@@ -49,11 +49,11 @@ begin
                 rs1 := to_integer(unsigned(Instr(19 downto 15)));
                 rs2 := to_integer(unsigned(Instr(24 downto 20)));
                 funct7 := Instr(31 downto 25);
-                
+                write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);
                 case func3 is
                     when F3_ADD =>      -- F3_ADD and F3_SUB have the same value "000"
-                        if funct7 = F7_ADD then      ADD_exec(rd, rs1, rs2, reg, mem, pc);
-                        elsif funct7 = F7_SUB then   SUB_exec(rd, rs1, rs2, reg, mem, pc);
+                        if funct7 = F7_ADD then      ADD_exec(rd=>rd, rs1=>rs1, rs2=>rs2, reg=>reg, mem=>mem, pc=>pc);
+                        elsif funct7 = F7_SUB then   SUB_exec(rd=>rd, rs1=>rs1, rs2=>rs2, reg=>reg, mem=>mem, pc=>pc);
                         else                                -- cover invalid cases
                             assert FALSE
                             report "Illegal Operation -- ADD | SUB"
@@ -91,7 +91,7 @@ begin
                 rs1 := to_integer(unsigned(Instr(19 downto 15)));
                 imm(10 downto 0) := Instr(30 downto 20);
                 imm(31 downto 11) := (others => Instr(31));
-                
+                write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);
                 case func3 is
                     -- basic I-Type
                     when F3_ADDI   => ADDI_exec(rs1 => rs1, rd => rd, imm => imm, mem => mem, reg => reg, pc => pc);                
@@ -117,8 +117,7 @@ begin
                             assert FALSE
                             report "Illegal Operation -- OP-IMM"
                             severity error;
-                end case;
-                write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);
+                end case;                
                  
             when OP_LOAD =>
                 func3 := Instr(14 downto 12);
@@ -126,7 +125,7 @@ begin
                 rs1 := to_integer(unsigned(Instr(19 downto 15)));
                 imm(10 downto 0) := Instr(30 downto 20);
                 imm(31 downto 11) := (others => Instr(20));
-                
+                write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);
                 case func3 is
                     when F3_LB      => LB_exec(rd => rd, rs1 => rs1, imm => imm, reg => reg, mem => mem, pc => pc);
                     when F3_LH      => LH_exec(rd => rd, rs1 => rs1, imm => imm, reg => reg, mem => mem, pc => pc);
@@ -138,16 +137,15 @@ begin
                             report "Illegal Operation -- OP-LOAD"
                             severity error;
                 end case;
-                write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);
-            when OP_JALR => 
+                
+            when OP_JALR =>                 
                 rd := to_integer(unsigned(Instr(11 downto 7)));
                 rs1 := to_integer(unsigned(Instr(19 downto 15)));
                 imm(10 downto 1) := Instr(30 downto 21);
                 imm(31 downto 11) := (others => Instr(31));      
-                write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);          
-                JALR_exec(rs1, rd, imm, mem, reg, pc);               
-                
-            -- end I-Type Instructions
+                write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);        
+                JALR_exec(rs1=>rs1, rd=>rd, imm=>imm, mem=>mem, reg=>reg, pc=>pc);
+                -- end I-Type Instructions
             -----------------------------------------------------------------------
             -- S-Type Instructions
             when OP_STORE =>
@@ -158,7 +156,7 @@ begin
                 imm(4 downto 0) := Instr(11 downto 7);
                 imm(10 downto 5) := Instr(30 downto 25);
                 imm(31 downto 11) := (others => Instr(31));
-                
+                write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);
                 case func3 is
                     when F3_SB  => SB_exec(rs1 => rs1, rs2 => rs2, imm => imm, reg => reg, mem => mem, pc => pc);
                     when F3_SH  => SH_exec(rs1 => rs1, rs2 => rs2, imm => imm, reg => reg, mem => mem, pc => pc);
@@ -167,8 +165,7 @@ begin
                             assert FALSE
                             report "Illegal Operation -- OP-STORE"
                             severity error;
-                end case;
-                write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);
+                end case;                
             -- end S-Type Instructions
             -----------------------------------------------------------------------
             -- U-Type Instructions
@@ -195,7 +192,7 @@ begin
                 imm(19 downto 12)   := Instr(19 downto 12);
                 imm(31 downto 20)   := (others => Instr(31));
                 write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);
-                JAL_exec(rd, imm, mem, reg, pc);
+                JAL_exec(rd=>rd, imm=>imm, mem=>mem, reg=>reg, pc=>pc);
             -- end J-Type Instructions
             -----------------------------------------------------------------------
             -- B-Type Instructions
@@ -208,24 +205,24 @@ begin
                 imm(10 downto 5) := Instr(30 downto 25);
                 imm(11) := Instr(7);
                 imm(31 downto 12) := (others => Instr(31));
-                
+                write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);
                 case func3 is
-                    when F3_BEQ  => BEQ_exec (rs1, rs2, imm, mem, reg, pc);               
-                    when F3_BNE  => BNE_exec (rs1, rs2, imm, mem, reg, pc);               
-                    when F3_BLT  => BLT_exec (rs1, rs2, imm, mem, reg, pc);
-                    when F3_BGE  => BGE_exec (rs1, rs2, imm, mem, reg, pc);
-                    when F3_BLTU => BLTU_exec (rs1, rs2, imm, mem, reg, pc);
-                    when F3_BGEU => BGEU_exec (rs1, rs2, imm, mem, reg, pc);
+                    when F3_BEQ  => BEQ_exec (rs1=>rs1, rs2=>rs2, imm=>imm, mem=>mem, reg=>reg, pc=>pc);               
+                    when F3_BNE  => BNE_exec (rs1=>rs1, rs2=>rs2, imm=>imm, mem=>mem, reg=>reg, pc=>pc);               
+                    when F3_BLT  => BLT_exec (rs1=>rs1, rs2=>rs2, imm=>imm, mem=>mem, reg=>reg, pc=>pc);
+                    when F3_BGE  => BGE_exec (rs1=>rs1, rs2=>rs2, imm=>imm, mem=>mem, reg=>reg, pc=>pc);
+                    when F3_BLTU => BLTU_exec (rs1=>rs1, rs2=>rs2, imm=>imm, mem=>mem, reg=>reg, pc=>pc);
+                    when F3_BGEU => BGEU_exec (rs1=>rs1, rs2=>rs2, imm=>imm, mem=>mem, reg=>reg, pc=>pc);
                     when others =>                      -- covers invalid cases
                         assert FALSE 
                         report "Illegal Operation -- OP_BRANCH" 
                         severity error;
-                end case;
-                write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);
+                end case;                
             -- end B-Type Instructions
             -----------------------------------------------------------------------
 
             when others =>          -- covers invalid cases
+                write_instruction_trace(l=>l, reg=>reg, instr=>instr, pc=>pc);  --still trace this
                 assert FALSE
                 report "Illegal Operation"
                 severity error;
