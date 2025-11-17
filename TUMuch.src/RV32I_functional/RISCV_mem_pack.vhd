c8593ed4055d95f4f35c1e66d0fda121089da3ef
Severin Hanf <Severin.Hanf@gmail.com>
2025-11-17 21:09:56 +0100

    smaller fixes. Add trace to gitignore

--- TUMuch.src/RV32I_functional/RISCV_mem_pack.vhd
+++ TUMuch.src/RV32I_functional/RISCV_mem_pack.vhd
@@ -388,7 +388,7 @@ package body mem_pack is
         return mem;                    --returning memory filled with binary stream for executing instructions
     end function;
 
-    procedure memory_data_dump (mem : MemType; file DataDumpFile : text) is         --evtl adresse und größe des dumps auswählba machen
+    procedure memory_data_dump (mem : MemType; file DataDumpFile : text) is         --evtl adresse und grï¿½ï¿½e des dumps auswï¿½hlba machen
         variable l : line;
     begin
         write( l , string'("ADDR | HEX      | BIN"));
@@ -396,12 +396,12 @@ package body mem_pack is
         write( l , string'("---------------------------------------------------"));
         writeline(DataDumpFile, l);
 
-        for i in 0 to 2**MemAddrSize-1 loop
-            write( l , hex_image(bit_vector(to_unsigned(i, 14)), 4) );
+        for i in 0 to 2**AddrSize-1 loop
+            write( l , hex_image(bit_vector(to_unsigned(i*4, 16)), 4) );
             write( l , string'(" | ") );
             write( l , hex_image(mem(i), 8), left, 8);
             write( l , string'(" | ") );
-            --write( l , to_string(mem(i)) );
+            write( l , to_string(mem(i)) );
 
             writeline(DataDumpFile, l);
         end loop;
