098c369a864407e9e3a4447066781617585d8806
Severin Hanf <Severin.Hanf@gmail.com>
2025-11-28 14:09:00 +0100

    add first empty files

---
+++ TUMuch.src/RV32I_rtl/RISCV_shifter.vhd
@@ -0,0 +1,18 @@
+-- created by Severin Hanf
+
+entity shifter is
+end shifter;
+
+architecture rtl of shifter is
+begin
+
+end rtl;
+
+
+entity shifter_Testbench is
+end shifter_Testbench;
+
+architecture Behavorial of shifter_Testbench is
+begin
+
+end Behavorial;
---
+++ TUMuch.src/RV32I_rtl/RISCV_system.vhd
@@ -0,0 +1,10 @@
+library work; 
+use work.defs_pack; 
+
+entity system is
+end;
+
+architecture rtl of system is
+begin
+
+end;
