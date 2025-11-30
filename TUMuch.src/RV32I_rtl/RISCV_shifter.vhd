f2b87f64dc86161cc3915dc514c7c4098991446a
Severin Hanf <Severin.Hanf@gmail.com>
2025-11-30 22:49:01 +0100

    prevent latch generation

--- TUMuch.src/RV32I_rtl/RISCV_shifter.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_shifter.vhd
@@ -19,20 +19,22 @@ architecture rtl of shifter32 is
 begin
     process(data_in, direction, shamt, arithmetic)
         variable data_tmp : BusDataType := (others => '0');
-        variable shamt_int : integer; 
+        variable shamt_int : integer := 0;
     begin 
         shamt_int := to_integer(unsigned(shamt));
         if shamt_int = 0 then 
-            data_out <= data_in;
+            data_out <= data_in;            
         else        
-            if direction = '0' then    --left shift                                                  --left shift
+            if direction = '0' then         --left shift   
+                data_tmp := (others=>'0');  --prevent interferring latches                                        
                 data_tmp(31 downto shamt_int)  := data_in((31 - shamt_int) downto 0);
                 data_tmp(shamt_int-1 downto 0) := (others=>'0');
-            else                        --right shift                                                                     --right shift
+            else                            --right shift     
+                data_tmp := (others=>'0');  --prevent interferring latches                                                   
                 if arithmetic = '1' then
-                    data_tmp(31 downto (31 - shamt_int)) := (others=>data_in(31));              --fill with msb
+                    data_tmp(31 downto (31 - shamt_int)) := (others=>data_in(31));   --fill with msb
                 else 
-                    data_tmp(31 downto (31 - shamt_int)) := (others=>'0');      --fill with zeros
+                    data_tmp(31 downto (31 - shamt_int)) := (others=>'0');           --fill with zeros
                 end if;            
                 data_tmp((31 - shamt_int) downto 0) := data_in(31 downto shamt_int);
             end if;
