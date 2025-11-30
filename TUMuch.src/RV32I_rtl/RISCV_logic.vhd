8837e2dae64903177b7808e3061e9941d120e1d5
Severin Hanf <Severin.Hanf@gmail.com>
2025-11-30 19:14:36 +0100

    add and - nand is actually no part of RISCV spec - skip this. add clkgen to testbench.

--- TUMuch.src/RV32I_rtl/RISCV_logic.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_logic.vhd
@@ -1,6 +1,8 @@
 -- created by JEONGJOO LIM
 library IEEE;
 use IEEE.numeric_bit.all;
+library work; 
+use work.defs_pack.all;
 
 --- OR ----
 
@@ -53,3 +55,23 @@ begin
     end process;
 end rtl;
 
+--- AND ---
+entity and32 is
+    port(
+        --x1, x2 : in  BusDataType; 
+        --y      : out BusDataType; 
+        x1, x2 : in  bit_vector(31 downto 0); 
+        y      : out bit_vector(31 downto 0); 
+        clk    : in  bit
+    );
+end and32; 
+
+architecture rtl of and32 is
+begin 
+    process(clk)
+    begin
+        if clk'event and clk = '1' then 
+            y <= x1 and x2;
+        end if; 
+    end process;    
+end rtl; 
--- TUMuch.src/RV32I_rtl/RISCV_logic_Testbench.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_logic_Testbench.vhd
@@ -1,29 +1,46 @@
 -- created by JEONGJOO LIM
 library IEEE;
 use IEEE.numeric_bit.all;
+library work; 
+use work.defs_pack.all; 
 
 entity logic_Testbench is
     port(
-        x1 : out bit_vector( 31 downto 0 );
-        x2 : out bit_vector( 31 downto 0 )
+        x1  : out bit_vector( 31 downto 0 );
+        x2  : out bit_vector( 31 downto 0 ); 
+        clk : out bit := '0' 
     );
 end logic_Testbench;
 
-architecture Behavioral of logic_Testbench is
+architecture stimul of logic_Testbench is 
+    
 begin
-    process
+    
+    clkgen : process
+    variable clk_tmp : bit := '0';
+    begin
+        wait for clkCycle/2;
+        clk_tmp := not clk_tmp; 
+        clk <= clk_tmp;          
+    end process; 
+    
+    inputgen : process
     begin
         x1 <= "01010101010101010101010101010101";
         x2 <= "10101010101010101010101010101010";
-        wait for 1 ns;
+        wait for clkCycle;
         
         x1 <= "00000000000000000000000000000000";
         
-        wait for 1 ns;
+        wait for clkCycle;
+        
+        x1 <= "11111111111111111111111111111111";
+        
+        wait for clkCycle;
             
         wait;
     end process;
-end Behavioral;
+end stimul;
 
 ------------------------ testbench TLE ---------------------------
 
@@ -32,14 +49,13 @@ entity logic_Testbench_TLE is
 end logic_Testbench_TLE;
 
 architecture Behavioral of logic_Testbench_TLE is
-
     signal x1_sig, x2_sig     : bit_vector(31 downto 0);
-    signal y_or_sig, y_xor_sig, y_nor_sig : bit_vector(31 downto 0);
-
+    signal y_or_sig, y_xor_sig, y_nor_sig, y_and_sig : bit_vector(31 downto 0);
+    signal clk_sig : bit := '0';
 begin
 
-    TB   : entity work.logic_Testbench(Behavioral)
-           port map (x1 => x1_sig, x2 => x2_sig);
+    TB   : entity work.logic_Testbench(stimul)
+           port map (clk=>clk_sig, x1 => x1_sig, x2 => x2_sig);
 
     UUT1 : entity work.or32(rtl)
            port map (x1 => x1_sig, x2 => x2_sig, y => y_or_sig);
@@ -49,5 +65,8 @@ begin
 
     UUT3 : entity work.nor32(rtl)
            port map (x1 => x1_sig, x2 => x2_sig, y => y_nor_sig);
+           
+    UUT4 : entity work.and32(rtl)
+           port map (clk=>clk_sig, x1 => x1_sig, x2 => x2_sig, y => y_and_sig);
 
 end Behavioral;
