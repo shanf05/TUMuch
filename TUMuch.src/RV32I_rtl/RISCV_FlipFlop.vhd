f120812247d1f6d5767e0bef1918497fe06cc49e
MaxTum1012 <max.biricz@gmail.com>
2025-12-05 18:17:48 +0100

    Added FlipFlop with flexible datasize inputs + enable + SR AR NR

--- TUMuch.src/RV32I_rtl/RISCV_FlipFlop.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_FlipFlop.vhd
@@ -1,29 +1,53 @@
 --created by Max Biricz
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
+library work;
+use work.defs_pack.all;
 
-
---FlipFlop with synchronous reset and enable
-entity D_FFRE is
-    Port (D : in bit;
+--Generic/Flexible D-FlipFlop with enable function
+entity D_FFG is 
+    generic(clk_edge, rst_level, has_rst, sync_rst : bit := '1';
+            size : integer := RegDataSize);
+    Port (D : in bit_vector(size-1 downto 0);
           RST : in bit;
-          EN : in bit;
           CLK : in bit;
-          Q: out bit);
-end D_FFRE;
+          EN : in bit;
+          Q: out bit_vector(size-1 downto 0));
+end D_FFG;
 
-architecture RTL of D_FFRE is
+architecture RTL of D_FFG is
 begin
-    process
-    begin
-        wait until clk = '1';
-        if RST = '1' then
-            Q <= '0';
-        else
-            if EN = '1' then
+    sr: if has_rst = '1' and sync_rst = '1' generate --synchronous read FlipFlop
+        process(clk, rst)
+        begin
+            if clk = clk_edge and CLK'event and en ='1' then
+                if RST = rst_level then
+                    Q <= (others => '0');
+                else
+                    Q <= D;
+                end if;
+            end if;
+         end process;
+     end generate;
+     
+     ar: if has_rst = '1' and sync_rst = '0' generate
+        process(clk, rst)
+        begin
+            if RST = rst_level and en = '1' then
+                Q <= (others => '0');
+            elsif CLK = clk_edge and CLK'event and en = '1' then
                 Q <= D;
             end if;
-        end if;
-    end process;
+        end process;
+     end generate; 
+     
+     nr: if has_rst = '0' generate
+     process
+     begin
+        wait until CLK = clk_edge;
+            Q <= D;
+     end process;
+     end generate;
+    
 end RTL;
 
--- TUMuch.src/RV32I_rtl/RISCV_FlipFlop_Testbench.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_FlipFlop_Testbench.vhd
@@ -2,13 +2,15 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_bit.all;
+library work;
+use work.defs_pack.all;
 
 -- clock signal generator
 entity clk_gen is
     generic(init_value : bit := '1';
             init_delay : time := 1 ns;
             T_high, T_low : time := 1 ns;
-            T_active : time := 200 ns
+            T_active : time := 2000 ns
                                     );
      port (clk : buffer bit := init_value);                               
 end clk_gen;
@@ -22,7 +24,7 @@ begin
        '0' after T_high
            when clk = '1' and now > 0 ns and now < T_active else
            clk;
-    
+   
 end architecture dataflow;
 
 -- reset signal generator
@@ -42,23 +44,35 @@ begin
            not rst_level after init_delay + 4 * T_rst;
 end dataflow;
 
--- Testbench for D-FlipFlop with Reset and Enable
-entity D_FFRE_Testbench is
-    Port (D : out bit;
-          EN : out bit);
-end D_FFRE_Testbench;
+library work;
+use work.defs_pack.all;
 
-architecture behavioral of D_FFRE_Testbench is
+-- Testbench for D-FlipFlop with Reset and Enable
+entity D_FFG_Testbench is
+    Port (D : out bit_vector(RegDatasize-1 downto 0);
+          EN : out bit);
+end D_FFG_Testbench;
+
+architecture behavioral of D_FFG_Testbench is
 begin
     process
     begin
-        D <= '1';
+        D <= X"AAAAAAAA";
         EN <= '1';
         wait for 4 ns;
-        D <= '0';
+        D <= X"DEADBEEF";
         wait for 5 ns;
         EN <= '0';
-        D <= '1';
+        D <= X"11223344";
+        wait for 5 ns;
+        EN <= '1';
+        D <= X"01010101";
+        EN <= '1';
+        wait for 4 ns;
+        D <= X"FFFFFFFF";
+        wait for 5 ns;
+        EN <= '0';
+        D <= X"12345678";
         wait for 5 ns;
         EN <= '1';
     end process;
@@ -66,11 +80,13 @@ begin
 end behavioral;
 
 ------------D_FFRE Top Level Entity
-entity D_FFRE_TLE is
-end D_FFRE_TLE;
+library work;
+use work.defs_pack.all;
 
+entity D_FFG_TLE is
+end D_FFG_TLE;
 
-architecture Behavioral of D_FFRE_TLE is
+architecture Behavioral of D_FFG_TLE is
     component clk_gen is
     port(clk : out bit);
     end component;
@@ -79,7 +95,7 @@ architecture Behavioral of D_FFRE_TLE is
     port(rst : out bit);
     end component;
     
-    component D_FFRE is
+    component D_FFG is
     port (D : in bit;
           EN : in bit;
           CLK : in bit;
@@ -87,16 +103,16 @@ architecture Behavioral of D_FFRE_TLE is
           Q : out bit  );
     end component;
     
-    component D_FFRE_Testbench is
+    component D_FFG_Testbench is
     port(D  : out bit;
          EN : out bit  );
     end component;
 
-signal D_sig, EN_sig, CLK_sig, RST_sig : bit;
-signal Q_sig : bit;
+signal EN_sig, CLK_sig, RST_sig : bit;
+signal D_sig, Q_sig1, Q_sig2, Q_sig3 : bit_vector(RegDataSize-1 downto 0);
     
 begin
-    TB : entity work.D_FFRE_Testbench(Behavioral)
+    TB : entity work.D_FFG_Testbench(Behavioral)
          port map(en => EN_sig, D => D_sig);
          
     CLK : entity work.clk_gen(dataflow)
@@ -105,8 +121,16 @@ begin
     RST : entity work.rst_gen(dataflow)
           port map(rst => rst_sig);
     
-    UUT : entity work.D_FFRE(RTL) 
-          port map(D => D_sig, EN => EN_sig, CLK => clk_sig, rst => rst_sig, Q => Q_sig);
-
+    DFF_SR : entity work.D_FFG(RTL)
+          generic map(has_rst => '1', sync_rst => '1') 
+          port map(D => D_sig, EN => EN_sig, CLK => clk_sig, rst => rst_sig, Q => Q_sig1);
+    
+    DFF_AR : entity work.D_FFG(RTL)
+          generic map(has_rst => '1', sync_rst => '0') 
+          port map(D => D_sig, EN => EN_sig, CLK => clk_sig, rst => rst_sig, Q => Q_sig2);
+          
+    DFF_NR : entity work.D_FFG(RTL)
+          generic map(has_rst => '0')
+          port map (D => D_sig, EN => EN_sig, clk => clk_sig, rst => rst_sig, Q => Q_sig3);
 end Behavioral;
 
