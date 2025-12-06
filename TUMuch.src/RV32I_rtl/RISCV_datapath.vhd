2268bb3aa15d0a3882f1cf8d0d05ff81707736cc
임정주 <mr4117@outlook.kr>
2025-12-06 19:17:35 +0100

    Datapath restructure
    Readjusted structure to RISCV, revised nomenclature
    FC now obsolete

--- TUMuch.src/RV32I_rtl/RISCV_datapath.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_datapath.vhd
@@ -11,48 +11,70 @@ entity Datapath is
         rst : in bit;
         
         -- data output
-        data_out  : out BusDataType; -- requires buffer
+        data_out  : out BusDataType; -- requires buffer, to Memory
         
-        -- from controller
-        d_in_mux  : in bit;
-        fc_sel    : in bit; 
-        reg_en    : in bit;                            -- to register_file
-        sel_in    : in RegAddrType;                    -- to register_file
-        sel_out_a : in RegAddrType;                    -- to register_file
-        sel_out_b : in RegAddrType;                    -- to register_file
-        operation : in bit_vector(2 downto 0);         -- to alu, see alu implementation -> 5 bits for 32 instructions
-        data_in   : in BusDataType                     -- to flow controller
+        -- from datapath:        
+        addr_in   : out BusDataType;
+        sel_mux_1 : in  bit;
+        sel_mux_2 : in  bit;
+        sel_mux_3 : in  bit; 
         
+        reg_en    : in  bit;
+        sel_in    : in  bit_vector(4 downto 0);
+        sel_out_a : in  bit_vector(4 downto 0);
+        sel_out_b : in  bit_vector(4 downto 0);
+        operation : in  bit_vector(2 downto 0);        
         
+        const_1   : in  BusDataType;
+        const_2   : in  BusDataType
     );
 end Datapath;
 
 architecture RTL of Datapath is
     
-    signal alu_in  : BusDataType;
-    signal alu_res : BusDataType;
+    signal rs_1 : BusDataType;
+    signal rs_2 : BusDataType;
     
-    signal rf_in   : BusDataType; 
+    signal in_mux1  : bit_vector(2*BusDataSize-1 downto 0);
+    signal out_mux1 : BusDataType;
     
-    signal data_out_sig : BusDataType;
+    signal in_mux2  : bit_vector(2*BusDataSize-1 downto 0);
+    signal out_mux2 : BusDataType;
+    
+    signal alu_result : BusDataType;
+    
+    signal in_mux3  : bit_vector(2*BusDataSize-1 downto 0);
+    signal out_mux3 : BusDataType;
     
 begin
-    alu : entity work.alu32 port map(
-        operand_a => alu_in,        --rs1
-        operand_b => data_out_sig,  --rs2
+    in_mux1 <= rs_1 & const_1;
+    MUX1 : entity work.mux generic map (ports => 2) port map (
+        input => in_mux1,
+        sel => sel_mux1,            -- should be fixed when mux sel is bit_vector
+        output => out_mux1
+    );
+    
+    in_mux2 <= rs_2 & const_2;
+    MUX2 : entity work.mux generic map (ports => 2) port map (
+        input => in_mux2,
+        sel => sel_mux2,            -- should be fixed when mux sel is bit_vector
+        output => out_mux2
+    );
+    
+    ALU : entity work.alu32 port map(
+        operand_a => out_mux1,      -- rs1/const1
+        operand_b => out_mux2,      -- rs2/const2
         
         operation => operation,     -- should be fixed when mux sel is bit_vector
         
-        result => alu_res
+        result => alu_result
     );
     
-    fc : entity work.fc port map(
-        alu_res => alu_res,
-        d_in => data_in,
-        
-        fc_sel => fc_sel,
-        
-        rf_in => rf_in
+    in_mux3 <= alu_result & const_2;
+    MUX3 : entity work.mux generic map (ports => 2) port map (
+        input => in_mux3,
+        sel => sel_mux3,            -- should be fixed when mux sel is bit_vector
+        output => out_mux3
     );
     
     rf : entity work.reg_file port map(
@@ -61,13 +83,13 @@ begin
         
         we => reg_en,
         w_addr => sel_in,
-        w_data => rf_in,
+        w_data => out_mux3,
         
         re_addr_1 => sel_out_a,
-        r_data_1 => alu_in,         --rs1
+        r_data_1 => rs_1,           --rs1
         re_addr_2 => sel_out_b,
-        r_data_2 => data_out_sig    --rs2 
+        r_data_2 => rs_2            --rs2 
     );
     
-    data_out <= data_out_sig;
+    data_out <= rs_1;
 end RTL;
