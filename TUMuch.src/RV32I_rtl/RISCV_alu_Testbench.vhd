25b94c09701c710b589e9878f75cfe89ec2dd39e
임정주 <mr4117@outlook.kr>
2025-12-08 19:24:41 +0100

    ALU RTL+TB fix (hopefully)
    added comparator
    
    changed operation code length
    
    added mux
    
    adjusted tb

--- TUMuch.src/RV32I_rtl/RISCV_alu_Testbench.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_alu_Testbench.vhd
@@ -7,8 +7,9 @@ use work.defs_pack.all;
 entity ALU32_Testbench is
     port(
         operand_a, operand_b : out BusDataType; 
-        operation            : out bit_vector(2 downto 0); 
-        result               : in  BusDataType  
+        operation            : out bit_vector(3 downto 0) 
+--        result               : in  BusDataType; 
+--        comp_result          : in  bit_vector(2 downto 0)   
     );
 end ALU32_Testbench;
 
@@ -16,17 +17,19 @@ architecture stimul of ALU32_Testbench is
     type operand_array is array (natural range <>) of bit_vector(31 downto 0);
 
     constant test_operands_a : operand_array := (
-        "01010101010101010101010101010101",
-        "11111111111111111111111111111111",
-        "00000000000000000000000000000000",
-        "10101010101010101010101010101010"
+        X"11111111",
+        X"FFFFFFFF",
+        X"00000000",
+        "10101010101010101010101010101010",
+        X"DEADBEEF"
     );
 
     constant test_operands_b : operand_array := (
         "00000000000000000000000000000001",
-        "00000000000000000000000000000001",
+        "00000000000000000000000000000010",
         "11111111111111111111111111111111",
-        "01010101010101010101010101010101"
+        "01010101010101010101010101010101",
+        X"DEADBEEF"
     );
 
 begin
@@ -45,8 +48,8 @@ begin
             wait for clkCycle;
 
             -- Test all 8 ALU operations
-            for op in 0 to 7 loop
-                operation <= bit_vector(to_unsigned(op, 3));
+            for op in 0 to 9 loop
+                operation <= bit_vector(to_unsigned(op, 4));
                 wait for clkCycle;
             end loop;
 
@@ -70,13 +73,25 @@ end ALU32_TLE;
 
 architecture Behavioral of ALU32_TLE is
     signal operand_a_sig, operand_b_sig, result_sig : BusDataType;
-    signal operation_sig : bit_vector(2 downto 0);
+    signal operation_sig : bit_vector(3 downto 0);
+    signal comp_result_sig : bit_vector(1 downto 0);
 begin
 
     TB   : entity work.ALU32_Testbench(stimul)
-            port map (operand_a => operand_a_sig, operand_b => operand_b_sig, operation => operation_sig, result => result_sig);
+            port map (
+                operand_a => operand_a_sig, 
+                operand_b => operand_b_sig, 
+                operation => operation_sig
+            );
 
     UUT1 : entity work.alu32(rtl)
-            port map(operand_a => operand_a_sig, operand_b => operand_b_sig, operation => operation_sig, result => result_sig);
+            port map(
+                operand_a => operand_a_sig, 
+                operand_b => operand_b_sig, 
+                operation => operation_sig, 
+                
+                result => result_sig, 
+                comp_result => comp_result_sig
+            );
 
 end;
