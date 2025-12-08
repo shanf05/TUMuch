2a80bd24224dff41449ed521a7cfdac03e4edb04
MaxTum1012 <max.biricz@gmail.com>
2025-12-08 20:25:50 +0100

    extended operation for alu to 4 bits in ALU, changed cmd_imm -> cmd_const & cmd_lui -> cmd_reg

--- TUMuch.src/RV32I_rtl/RISCV_ctrl_instr_dec.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_ctrl_instr_dec.vhd
@@ -9,14 +9,14 @@ use work.instr_dec_pack.all;
 --decoder
     --signals to ALU: OP
     --signals to RegFile: sel_in, sel_out_a, sel_out_b
-    --signal  to FSM: ctrl (cmd_stop, cmd_jmp, cmd_auipc, cmd_lui, cmd_load, cmd_const, cmd_calc, cmd_store, cmd_take_jmp)
+    --signal  to FSM: ctrl (cmd_stop, cmd_jmp, cmd_auipc, cmd_reg, cmd_load, cmd_const, cmd_calc, cmd_store, cmd_take_jmp)
 
 entity ctrl_instr_dec is
     Port (instr : in bit_vector(BusDataSize-1 downto 0);
           sel_in, sel_out_a, sel_out_b : out bit_vector(4 downto 0); --sel_out_a = rd, sel_out_b = rs1, sel_out_c = rs2
           pc_in : in bit_vector (AddrSize-1 downto 0);
           ctrl : out CtrlType;
-          op : out bit_vector(2 downto 0);
+          op : out bit_vector(3 downto 0);
           const_1 : out Immtype;
           const_2 : out Immtype                                             
           );
@@ -26,15 +26,15 @@ architecture RTL of ctrl_instr_dec is
 signal op_code : bit_vector(6 downto 0) := (others => '0');         --prevent Latch
 signal func3 : bit_vector(2 downto 0) := (others => '0');           --prevent Latch
 signal func7 : bit_vector(6 downto 0) := (others => '0');           --prevent Latch
-signal cmd_take_jmp, cmd_store, cmd_calc, cmd_imm, cmd_load, cmd_lui, cmd_auipc, cmd_jmp, cmd_stop : bit;
+signal cmd_take_jmp, cmd_store, cmd_calc, cmd_const, cmd_load, cmd_reg, cmd_auipc, cmd_jmp, cmd_stop : bit;
 begin
     op_code <= instr(6 downto 0);
     
     process
     begin
     -- default assignment of ctrl signals
-    cmd_take_jmp <='0'; cmd_store <='0'; cmd_calc <='0'; cmd_imm <='0'; cmd_load <='0';
-    cmd_lui <= '0'; cmd_auipc <='0'; cmd_jmp <='0'; cmd_stop <= '0';
+    cmd_take_jmp <='0'; cmd_store <='0'; cmd_calc <='0'; cmd_const <='0'; cmd_load <='0';
+    cmd_reg <= '0'; cmd_auipc <='0'; cmd_jmp <='0'; cmd_stop <= '0';
     func3 <= (others => '0'); func7 <= (others => '0'); op <= (others => '0');
     case op_code is
         -- R-Type Instructions
@@ -45,10 +45,10 @@ begin
             sel_out_a <= instr(19 downto 15);
             sel_out_b <= instr(24 downto 20);
             --ctrl table
-            --STOP, JMP, AUIPC, LUI, LOAD, IMM, CALC, STORE, TAKE_JMP
-            -- '0', '0',   '0', '0',  '0', '0',  '1',   '0',   '0'
+            --STOP, JMP, AUIPC, REG, LOAD, CONST, CALC, STORE, TAKE_JMP
+            -- '0', '0',   '0', '0',  '0',   '0',  '1',   '0',   '0'
             cmd_calc <= '1';
-            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_lui & cmd_load & cmd_imm & cmd_calc & cmd_store & cmd_take_jmp;
+            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_reg & cmd_load & cmd_const & cmd_calc & cmd_store & cmd_take_jmp;
             const_2 <= (others => '0');
             const_1(13 downto 0) <= pc_in (13 downto 0);
             const_1(31 downto 14) <= (others => '0');
@@ -77,9 +77,9 @@ begin
                 when F3_AND  =>
                     op <= ALU_AND;
                 when F3_SLT  =>
-                    op <= ALU_SUB;          --for command slt -> Subtract rs1 and rs2 and check if value is greater 0
+                    op <= ALU_SLT;         
                 when F3_SLTU =>
-                    op <= ALU_SUB;
+                    op <= ALU_SLTU;
                 when F3_SRL =>
                     func7 <= instr(31 downto 25);
                     case func7 is
@@ -98,14 +98,14 @@ begin
             sel_in <= instr(11 downto 7);
             sel_out_a <= instr(19 downto 15);
             sel_out_b <= (others => '0');
-            --STOP, JMP, AUIPC, LUI, LOAD, IMM, CALC, STORE, TAKE_JMP
-            -- '0', '0',   '0', '0',  '0', '1',  '1',   '0',   '0'
-            cmd_imm <= '1'; cmd_calc <= '1';
-            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_lui & cmd_load & cmd_imm & cmd_calc & cmd_store & cmd_take_jmp;
+            --STOP, JMP, AUIPC, REG, LOAD, CONST, CALC, STORE, TAKE_JMP
+            -- '0', '0',   '0', '0',  '0',   '1',  '1',   '0',   '0'
+            cmd_const <= '1'; cmd_calc <= '1';
+            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_reg & cmd_load & cmd_const & cmd_calc & cmd_store & cmd_take_jmp;
             const_2(11 downto 0) <= instr(31 downto 20);        --imm to ALU
             const_2(31 downto 12) <= (others => instr(31));     --imm to ALU
-            const_1(13 downto 0) <= pc_in (13 downto 0);
-            const_1(31 downto 14) <= (others => '0');           
+            const_1(13 downto 0) <= pc_in (13 downto 0);        --pc in const_1
+            const_1(31 downto 14) <= (others => '0');           --pc in const_1
             
             case func3 is
                 when F3_ADDI   =>
@@ -117,9 +117,9 @@ begin
                 when F3_AND    => 
                     op <= ALU_AND;
                 when F3_SLT    => 
-                    op <= ALU_SUB;
+                    op <= ALU_SLT;
                 when F3_SLTU   =>
-                    op <= ALU_SUB;
+                    op <= ALU_SLTU;
                 when F3_SLL  =>
                     op <= ALU_SLL;
                 when F3_SRL =>
@@ -142,10 +142,10 @@ begin
             sel_out_a <= instr(19 downto 15);
             sel_out_b <= (others => '0');
             op <= (others => '0');      --no ALU Operation needed
-            --STOP, JMP, AUIPC, LUI, LOAD, IMM, CALC, STORE, TAKE_JMP
+            --STOP, JMP, AUIPC, REG, LOAD, IMM, CALC, STORE, TAKE_JMP
             -- '0', '0',   '0', '0',  '1', '0',  '0',   '0',   '0'
             cmd_load <= '1';
-            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_lui & cmd_load & cmd_imm & cmd_calc & cmd_store & cmd_take_jmp;
+            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_reg & cmd_load & cmd_const & cmd_calc & cmd_store & cmd_take_jmp;
             const_2(11 downto 0) <= instr(31 downto 20);
             const_2(31 downto 0) <= (others => instr(31));
             const_1(13 downto 0) <= pc_in (13 downto 0);
@@ -168,10 +168,10 @@ begin
             sel_in <= (others => '0');
             sel_out_a <= instr(24 downto 20);       --rs1 and rs2 swtiched for store commands
             sel_out_b <= instr(19 downto 15);       --rs1 and rs2 swtiched for store commands
-            --STOP, JMP, AUIPC, LUI, LOAD, IMM, CALC, STORE, TAKE_JMP
-            -- '0', '0',   '0', '0',  '0', '0',  '0',   '1',   '0'
+            --STOP, JMP, AUIPC, REG, LOAD, CONST, CALC, STORE, TAKE_JMP
+            -- '0', '0',   '0', '0',  '0',   '0',  '0',   '1',   '0'
             cmd_store <= '1';
-            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_lui & cmd_load & cmd_imm & cmd_calc & cmd_store & cmd_take_jmp;
+            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_reg & cmd_load & cmd_const & cmd_calc & cmd_store & cmd_take_jmp;
             op <= (others => '0');                         --no ALU Operation needed
             const_1(4 downto 0) <= instr(11 downto 7);      --Only in Store instr: Immediate in const_1
             const_1(11 downto 5) <= instr(31 downto 25);    --Only in Store instr: Immediate in const_1
@@ -195,10 +195,10 @@ begin
             sel_out_b <= instr(24 downto 20);
             sel_in <= (others => '0');
             op <= (others => '0');                         --no ALU Operation needed
-            --STOP, JMP, AUIPC, LUI, LOAD, IMM, CALC, STORE, TAKE_JMP
-            -- '0', '1',   '0', '0',  '0', '0',  '1',   '0',   '1'
+            --STOP, JMP, AUIPC, REG, LOAD, CONST, CALC, STORE, TAKE_JMP
+            -- '0', '1',   '0', '0',  '0',   '0',  '1',   '0',   '1'
             cmd_jmp <= '1'; cmd_calc <= '1'; cmd_take_jmp <= '1';
-            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_lui & cmd_load & cmd_imm & cmd_calc & cmd_store & cmd_take_jmp;
+            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_reg & cmd_load & cmd_const & cmd_calc & cmd_store & cmd_take_jmp;
             const_2(3 downto 0) <= instr(11 downto 8);
             const_2(9 downto 4) <= instr(30 downto 25);
             const_2(10) <= instr(7);
@@ -225,10 +225,10 @@ begin
             sel_out_a <= (others => '0');
             sel_out_b <= (others => '0');
             op <= (others => '0');                                          --no ALU Operation needed
-            --STOP, JMP, AUIPC, LUI, LOAD, IMM, CALC, STORE, TAKE_JMP
-            -- '0', '0',   '0', '1',  '0', '0',  '0',   '0',   '0'   
-            cmd_lui <= '1';
-            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_lui & cmd_load & cmd_imm & cmd_calc & cmd_store & cmd_take_jmp;
+            --STOP, JMP, AUIPC, REG, LOAD, CONST, CALC, STORE, TAKE_JMP
+            -- '0', '0',   '0', '1',  '0',   '0',  '0',   '0',   '0'   
+            cmd_reg <= '1';
+            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_reg & cmd_load & cmd_const & cmd_calc & cmd_store & cmd_take_jmp;
             const_2(31 downto 12) <= instr(31 downto 12);
             const_2(11 downto 0) <= (others => '0');
             const_1(13 downto 0) <= pc_in (13 downto 0);
@@ -239,10 +239,10 @@ begin
             sel_out_a <= (others => '0');
             sel_out_b <= (others => '0');
             op <= (others => '0');                                          --no ALU Operation needed
-            --STOP, JMP, AUIPC, LUI, LOAD, IMM, CALC, STORE, TAKE_JMP
-            -- '0', '0',   '1', '0',  '0', '0',  '0',   '0',   '0'  
+            --STOP, JMP, AUIPC, REG, LOAD, CONST, CALC, STORE, TAKE_JMP
+            -- '0', '0',   '1', '0',  '0',   '0',  '0',   '0',   '0'  
             cmd_auipc <= '1';
-            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_lui & cmd_load & cmd_imm & cmd_calc & cmd_store & cmd_take_jmp;
+            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_reg & cmd_load & cmd_const & cmd_calc & cmd_store & cmd_take_jmp;
             const_2(31 downto 12) <= instr(31 downto 12);       --immediate to ALU
             const_2(11 downto 0) <= (others => '0');            --immediate to ALU
             const_1(13 downto 0) <= pc_in (13 downto 0);        --pc to ALU
@@ -255,10 +255,10 @@ begin
             sel_out_a <= (others => '0');
             sel_out_b <= (others => '0');
             op <= (others => '0');                                        --no ALU Operation needed            
-            --STOP, JMP, AUIPC, LUI, LOAD, IMM, CALC, STORE, TAKE_JMP
-            -- '0', '1',   '0', '0',  '0', '0',  '0',   '0',   '1'
+            --STOP, JMP, AUIPC, REG, LOAD, CONST, CALC, STORE, TAKE_JMP
+            -- '0', '1',   '0', '0',  '0',   '0',  '0',   '0',   '1'
             cmd_jmp <='1'; cmd_take_jmp<='1';
-            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_lui & cmd_load & cmd_imm & cmd_calc & cmd_store & cmd_take_jmp;
+            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_reg & cmd_load & cmd_const & cmd_calc & cmd_store & cmd_take_jmp;
             const_2(0) <= '0';                                      --immediate to ALU
             const_2(10 downto 1)    <= instr(30 downto 21);         --immediate to ALU
             const_2(11)             <= instr(20);                   --immediate to ALU
@@ -273,10 +273,10 @@ begin
             sel_out_a <= instr(19 downto 15);
             sel_out_b <= (others => '0');            
             op <= (others => '0');
-            --STOP, JMP, AUIPC, LUI, LOAD, IMM, CALC, STORE, TAKE_JMP
-            -- '0', '1',   '0', '0',  '0', '0',  '0',   '0',   '1'  
-            cmd_jmp <='1'; cmd_take_jmp <='1';
-            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_lui & cmd_load & cmd_imm & cmd_calc & cmd_store & cmd_take_jmp;
+            --STOP, JMP, AUIPC, REG, LOAD, CONST, CALC, STORE, TAKE_JMP
+            -- '0', '1',   '0', '1',  '0',   '0',  '0',   '0',   '1'  
+            cmd_jmp <='1'; cmd_take_jmp <='1'; cmd_reg <='1';
+            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_reg & cmd_load & cmd_const & cmd_calc & cmd_store & cmd_take_jmp;
             const_2(11 downto 0) <= instr(31 downto 20);            --immediate to ALU
             const_2(31 downto 12) <= (others => instr(31));         --immediate to ALU
             const_1(13 downto 0) <= pc_in (13 downto 0);      
@@ -289,10 +289,10 @@ begin
             sel_in <= (others => '0');
             sel_out_a <= (others => '0');
             sel_out_b <= (others => '0');
-            --STOP, JMP, AUIPC, LUI, LOAD, IMM, CALC, STORE, TAKE_JMP
+            --STOP, JMP, AUIPC, REG, LOAD, CONST, CALC, STORE, TAKE_JMP
             -- '1', '0',   '0', '0',  '0', '0',  '0',   '0',   '0'
             cmd_stop <='1';
-            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_lui & cmd_load & cmd_imm & cmd_calc & cmd_store & cmd_take_jmp;
+            ctrl <= cmd_stop & cmd_jmp & cmd_auipc & cmd_reg & cmd_load & cmd_const & cmd_calc & cmd_store & cmd_take_jmp;
             const_2 <= (others => '0');         --immediate to ALU
             
         when others =>
--- TUMuch.src/RV32I_rtl/RISCV_instr_dec_pack.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_instr_dec_pack.vhd
@@ -9,15 +9,17 @@ use work.defs_pack.all;
 package instr_dec_pack is
     
     --ALU Operation encoding
-    subtype ALUOp is bit_vector(2 downto 0); 
+    subtype ALUOp is bit_vector(3 downto 0); 
     
-    constant ALU_XOR : ALUOp := "000";
-    constant ALU_OR  : ALUOp := "001";
-    constant ALU_AND : ALUOp := "010";
-    constant ALU_ADD : ALUOp := "011";
-    constant ALU_SUB : ALUOp := "100";
-    constant ALU_SLL : ALUOp := "101";
-    constant ALU_SRL : ALUOp := "110";
-    constant ALU_SRA : ALUOp := "111";
+    constant ALU_XOR  : ALUOp := "0000";          --also valid for XORI
+    constant ALU_OR   : ALUOp := "0001";          --also valid for ORI
+    constant ALU_AND  : ALUOp := "0010";          --also valid for ANDI
+    constant ALU_ADD  : ALUOp := "0011";          --also valid for ADDI
+    constant ALU_SUB  : ALUOp := "0100";
+    constant ALU_SLL  : ALUOp := "0101";          --also valid for SLLI
+    constant ALU_SRL  : ALUOp := "0110";          --also valid for SRLI
+    constant ALU_SRA  : ALUOp := "0111";          --also valid for SRAI
+    constant ALU_SLT  : ALUOp := "1000";          --also valid for SLTI
+    constant ALU_SLTU : ALUOp := "1001";          --also valid for SLTIU
     
 end package;
