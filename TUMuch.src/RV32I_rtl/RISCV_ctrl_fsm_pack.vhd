3aef49881256c63753db70d2e7108913b0f4876d
Severin Hanf <Severin.Hanf@gmail.com>
2025-12-09 14:17:57 +0100

    remove unneccessary state

--- TUMuch.src/RV32I_rtl/RISCV_ctrl_fsm_pack.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_ctrl_fsm_pack.vhd
@@ -1,11 +1,10 @@
 -- erstellt von Severin Hanf
 package ctrl_fsm_pack is 
     subtype  StateType is bit_vector (4 downto 0); 
-    constant s_if   : StateType := "10000";         -- instruction fetch 
-    constant s_pfex : StateType := "01000";         -- parameter fetch (decode) and execute
-    constant s_mem  : StateTYpe := "00100";         -- memory write
-    constant s_rd   : StateType := "00010";         -- when memory read, one state more to store data in registers
-    constant s_stop : StateType := "00001";         -- fullstop (and fallback to stop)
+    constant s_if   : StateType := "1000";         -- instruction fetch 
+    constant s_pfex : StateType := "0100";         -- parameter fetch (decode) and execute
+    constant s_mem  : StateTYpe := "0010";         -- memory write
+    constant s_stop : StateType := "0001";         -- fullstop (and fallback to stop)
     
     -- MUX selects: 
     constant sel_mux_1_rs_2      : bit  := '0'; 
