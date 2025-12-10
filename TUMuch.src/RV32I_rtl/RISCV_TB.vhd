ffa9830717b0faec1af7519b7b9e4e1bcc723899
Josip Pepic <pepicjosip07@gmail.com>
2025-12-10 20:34:03 +0100

    TB fix

--- TUMuch.src/RV32I_rtl/RISCV_TB.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_TB.vhd
@@ -10,11 +10,15 @@ use std.textio.all;
 
 entity TB is
     port (
-        clk             : in bit;
-        data_to_mem     : out BusDataType;
+        active          : in bit;                               -- active signal from fsm
+        clk             : in bit;                               
         data_from_mem   : in BusDataType;
+        
+        rst             : out bit;                              -- used to "start" the cpu manually
+        data_to_mem     : out BusDataType;                      
         mem_addr        : out bit_vector(AddrSize-1 downto 0);
         w_en            : out bit;
+        acc_size        : out bit_vector(1 downto 0);
         
         sel             : out bit
         
@@ -27,12 +31,20 @@ architecture Behavioral of TB is
 begin
     process
         variable l : line;
+        variable w : line;
         variable v : string(1 to 32);
         variable success : boolean;
         variable addr   : integer range 0 to 2**MemAddrSize-1 := 0; 
     begin
+        
+        
+        rst <= '0';
+        
+        -------------------- Memory Load --------------------
+        
         sel <= '1';
         w_en <= '1';
+        acc_size <= acc_size_word;
         loop
             exit when endfile(BinFile);
             readline(BinFile, l);
@@ -40,36 +52,42 @@ begin
             data_to_mem <= Binary_to_data(v(1 to 32));
             mem_addr <= bit_vector(to_unsigned(addr, AddrSize));
             wait until clk'event and clk='1';
-            addr := addr +1;
+            addr := addr + 4;
+            report "done";
         end loop;
         w_en <= '0';
-        sel <= '0';
         
-        wait for 1000 ms;      -- wait for cpu to work off all instructions
+        -- wait for cpu to finish instructions 
+        rst <= '1';
+        wait for 2 ns;
+        rst <= '0';
+        wait until active = '0';
         
-        sel <= '1';
+        -------------------- Memory Dump --------------------
         
-        write( l , string'(" ADDR  |   HEX    |              BIN"));
-        writeline(DataDumpFile, l);
-        write( l , string'("-----------------------------------------------------"));
-        writeline(DataDumpFile, l);
+        write( w , string'(" ADDR  |   HEX    |              BIN"));
+        writeline(DataDumpFile, w);
+        write( w , string'("-----------------------------------------------------"));
+        writeline(DataDumpFile, w);
         
+        addr := 0;
         for i in 0 to 2**MemAddrSize-1 loop
-            mem_addr <= bit_vector(to_unsigned(i, AddrSize));
+            mem_addr <= bit_vector(to_unsigned(addr, AddrSize));
             
-            wait for 5 ns;
+            wait for 0.5 ns;
             
-            write( l , string'("0x") );            
-            write( l , hex_image_4(bit_vector(to_unsigned(i*4, 32))) );
-            write( l , string'(" | ") );
-            write( l , hex_image_8(data_from_mem), left, 8);
-            write( l , string'(" | ") );
-            write( l , func_to_string(data_from_mem));
+            write( w , string'("0x") );            
+            write( w , hex_image_4(bit_vector(to_unsigned(i*4, 32))) );
+            write( w , string'(" | ") );
+            write( w , hex_image_8(data_from_mem), left, 8);
+            write( w , string'(" | ") );
+            write( w , func_to_string(data_from_mem));
 
-            writeline(DataDumpFile, l);
+            writeline(DataDumpFile, w);
+            addr := addr +4;
         end loop;
         
-        
+        wait;
     end process;
 
 end Behavioral;
