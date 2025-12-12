dfebca1491219c2efac45cb7082851cfea3b2551
CraftJJGamer308 <mr4117@outlook.kr>
2025-12-12 15:03:31 +0100

    ALU process rewrite using default value assignments -> no effect on functionality

--- TUMuch.src/RV32I_rtl/RISCV_alu.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_alu.vhd
@@ -108,100 +108,42 @@ begin
     
     process(operation, operand_a, operand_b)
     begin
-        case operation is    
-            when "0000" =>   --xor(i)
-                o_mode_sig <= '0';          -- prevent latches 
-                dir_sig    <= '0';          -- prevent latches
-                arith_sig  <= '0';          -- prevent latches         
-                comp_is_signed_sig <= '0';  -- prevent latches       
-            when "0001" =>   --or(i)
-                o_mode_sig <= '0';          -- prevent latches 
-                dir_sig    <= '0';          -- prevent latches
-                arith_sig  <= '0';          -- prevent latches        
-                comp_is_signed_sig <= '0';  -- prevent latches
-            when "0010" =>   --and(i)
-                o_mode_sig <= '0';          -- prevent latches 
-                dir_sig    <= '0';          -- prevent latches
-                arith_sig  <= '0';          -- prevent latches         
-                comp_is_signed_sig <= '0';  -- prevent latches       
+        -- default assignments to prevent latches
+        o_mode_sig <= '0'; 
+        dir_sig    <= '0';
+        arith_sig  <= '0';         
+        comp_is_signed_sig <= '0';     
+
+        case operation is                 
+            when "0000" =>                      --xor(i) 
+            when "0001" =>                      --or(i)
+            when "0010" =>                      --and(i) 
+
+            when "0011" =>                      --add(i)
+            when "0100" =>                      --sub
+                o_mode_sig <= '1';      
+
+            when "0101" =>                      --sll(i)
+            when "0110" =>                      --srl(i)
+                dir_sig    <= '1';          
+            when "0111" =>                      --sra(i)
+                dir_sig    <= '1';          
+                arith_sig  <= '1';          
+            when "1000" =>                      --slt(i)
+                comp_is_signed_sig <= '1';             
+            when "1001" =>                      --slt(i)u
 
 
-            when "0011" =>   --add(i)
-                o_mode_sig <= '0';
-                dir_sig    <= '0';          -- prevent latches
-                arith_sig  <= '0';          -- prevent latches        
-                comp_is_signed_sig <= '0';  -- prevent latches
-            when "0100" =>   --sub
-                o_mode_sig <= '1';
-                dir_sig    <= '0';          -- prevent latches
-                arith_sig  <= '0';          -- prevent latches        
-                comp_is_signed_sig <= '0';  -- prevent latches
-
-
-            when "0101" =>   --sll(i)
-                o_mode_sig <= '0';          -- prevent latches
-                dir_sig    <= '0'; 
-                arith_sig  <= '0';        
-                comp_is_signed_sig <= '0';  -- prevent latches
-            when "0110" =>   --srl(i)
-                o_mode_sig <= '0';          -- prevent latches
-                dir_sig    <= '1';
-                arith_sig  <= '0';         
-                comp_is_signed_sig <= '0';  -- prevent latches
-            when "0111" =>   --sra(i)
-                o_mode_sig <= '0';          -- prevent latches
-                dir_sig    <= '1';          -- prevent latches
-                arith_sig  <= '1';          -- prevent latches
-                comp_is_signed_sig <= '0';  -- prevent latches
-
-
-            when "1000" =>   --slt(i)
-                o_mode_sig <= '0';          -- prevent latches
-                dir_sig    <= '0';
-                arith_sig  <= '0';
-                comp_is_signed_sig <= '1';         
-            when "1001" =>   --slt(i)u
-                o_mode_sig <= '0';          -- prevent latches
-                dir_sig    <= '0';          -- prevent latches
-                arith_sig  <= '0';          -- prevent latches
-                comp_is_signed_sig <= '0';
-            when "1010" =>   --beq
-                o_mode_sig <= '0';          -- prevent latches
-                dir_sig    <= '0';
-                arith_sig  <= '0';
-                comp_is_signed_sig <= '0';         
-            when "1011" =>   --bne
-                o_mode_sig <= '0';          -- prevent latches
-                dir_sig    <= '0';          -- prevent latches
-                arith_sig  <= '0';          -- prevent latches
-                comp_is_signed_sig <= '0';
-            -- when "1100" =>   --blt
-            --     o_mode_sig <= '0';          -- prevent latches
-            --     dir_sig    <= '0';
-            --     arith_sig  <= '0';
+            when "1010" =>                      --beq
+            when "1011" =>                      --bne
+            -- when "1100" =>                   --blt
             --     comp_is_signed_sig <= '1';         
-            when "1101" =>   --bge
-                o_mode_sig <= '0';          -- prevent latches
-                dir_sig    <= '0';          -- prevent latches
-                arith_sig  <= '0';          -- prevent latches
-                comp_is_signed_sig <= '1';
-            -- when "1110" =>   --bltu
-            --     o_mode_sig <= '0';          -- prevent latches
-            --     dir_sig    <= '0';
-            --     arith_sig  <= '0';
-            --     comp_is_signed_sig <= '1';         
-            when "1111" =>   -- bgeu
-                o_mode_sig <= '0';          -- prevent latches
-                dir_sig    <= '0';          -- prevent latches
-                arith_sig  <= '0';          -- prevent latches
-                comp_is_signed_sig <= '0';
-
+            when "1101" =>                      --bge
+                comp_is_signed_sig <= '1';  
+            -- when "1110" =>                   --bltu
+            when "1111" =>                      --bgeu
 
             when others => 
-                o_mode_sig <= '0';          -- prevent latches
-                dir_sig    <= '0';          -- prevent latches
-                arith_sig  <= '0';          -- prevent latches
-                comp_is_signed_sig <= '0';  -- prevent latches
         end case;
     end process; 
     
