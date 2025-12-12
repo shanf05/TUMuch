3b29d8b4d5d8c26c286aaaaad86897ada768fd13
CraftJJGamer308 <mr4117@outlook.kr>
2025-12-12 11:35:34 +0100

    change addsub operand order

--- TUMuch.src/RV32I_rtl/RISCV_alu.vhd
+++ TUMuch.src/RV32I_rtl/RISCV_alu.vhd
@@ -58,7 +58,7 @@ architecture rtl of alu32 is
     signal mux_sel_sig      : bit_vector(4 downto 0);
         
 begin    
-    addsub32  : entity work.addsub    port map(o_mode=>o_mode_sig, a=>operand_a, b=>operand_b, d_out=>res_addsub_sig);
+    addsub32  : entity work.addsub    port map(o_mode=>o_mode_sig, a=>operand_b, b=>operand_a, d_out=>res_addsub_sig);
     shifter32 : entity work.shifter32 port map(data_in=>operand_b, direction=>dir_sig, arithmetic=>arith_sig, shamt=>operand_a(4 downto 0), data_out=>res_shifter_sig);
     and32     : entity work.and32     port map(x1=>operand_a, x2=>operand_b, y=>res_and_sig); 
     or32      : entity work.or32      port map(x1=>operand_a, x2=>operand_b, y=>res_or_sig); 
